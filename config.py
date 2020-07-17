0b849ac9bedeffaee20688d6c8259b1ab0d48e74
Bushi <kujtim.dokaj@mail.utoronto.ca>
2020-07-16 22:56:52 -0400

    added files for heroku deployment

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     TEMPLATES_AUTO_RELOAD = True
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
