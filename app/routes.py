5ead68720f9552a12ae009724ccfec19893f5975
Bushi <kujtim.dokaj@mail.utoronto.ca>
2020-07-17 23:04:25 -0400

    updated app to allow for multiple game requests

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,53 @@
 from app import app
-from flask import render_template, request, jsonify, make_response, redirect, url_for
+from flask import render_template, request, jsonify, make_response, redirect, url_for, session
+import pickle
+from uuid import uuid4
 from logic.chess_board import Board
 from logic.chess_pieces import *
+from cachelib.simple import SimpleCache
 
-b = Board()
+c = SimpleCache()
+
 
 @app.route('/', methods=["GET", "POST"])
 @app.route('/chess', methods=["GET", "POST"])
 def chess():
-    flipped = b.flipped
+    b = load_board()
 
+    flipped = b.flipped
     img_dict = b.board_html()
+
+    save_board(b)
 
     return render_template('base.html', img_dict=img_dict, flipped=flipped)
 
 
 @app.route('/flip')
 def flip():
+    b = load_board()
+
     b.flipped = not b.flipped
+
+    save_board(b)
+
     return redirect(url_for('chess'))
 
 
 @app.route('/new_game', methods=["GET", "POST"])
 def new_game():
+    b = load_board()
+
     b.reset()
+
+    save_board(b)
 
     return redirect(url_for('chess'))
 
 
 @app.route('/moves', methods=["POST"])
 def load_moves():
+    b = load_board()
+
     moves = []
     if request.method == 'POST':
 
@@ -46,6 +64,8 @@ def load_moves():
 
 @app.route('/promote', methods=['GET', 'POST'])
 def promote():
+    b = load_board()
+
     error = False
 
     sq_one = eval(request.get_json()['sq_one'])
@@ -54,6 +74,7 @@ def promote():
 
     try:
         b.move(sq_one, sq_two, promotion)
+        save_board(b)
     except Exception as e:
         error = str(e)
 
@@ -62,6 +83,8 @@ def promote():
 
 @app.route('/execute', methods=['GET', 'POST'])
 def execute():
+    b = load_board()
+
     if request.method == "POST":
 
         castle = None
@@ -89,8 +112,36 @@ def execute():
             if b.game_over():
                 outcome = b.outcome
             empty = b.js_remove()
+            save_board(b)
         except Exception as e:
             error = str(e)
         response = {'error': error, 'castle': castle, 'empty': empty, 'outcome': outcome}
 
     return make_response(jsonify(response))
+
+
+def generate_id():
+    return uuid4().__str__()
+
+
+def load_board():
+
+    if 'game_id' in session:
+        pb = c.get(session['game_id'])
+        board = pickle.loads(pb)
+    else:
+        # Initialize new board
+        board = Board()
+
+    return board
+
+
+def save_board(board):
+    pb = pickle.dumps(board)
+
+    if 'game_id' in session:
+        c.set(session['game_id'], pb)
+    else:
+        unique_id = generate_id()
+        session['game_id'] = unique_id
+        c.set(unique_id, pb)
